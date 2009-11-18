91b693499a57ba44c6a314fbcb03117458086bc9
tekkub <tekkub@gmail.com>
2009-11-18 02:49:34 -0700

    Add disenchant button, canNeed/canDE logics

--- teksLoot.lua
+++ teksLoot.lua
@@ -16,7 +16,7 @@ local function HideTip() GameTooltip:Hide() end
 local function HideTip2() GameTooltip:Hide(); ResetCursor() end
 
 
-local rolltypes = {"need", "greed", [0] = "pass"}
+local rolltypes = {"need", "greed", "disenchant", [0] = "pass"}
 local function SetTip(frame)
 	GameTooltip:SetOwner(frame, "ANCHOR_RIGHT")
 	GameTooltip:SetText(frame.tiptext)
@@ -87,7 +87,7 @@ end
 
 local function CreateRollFrame()
 	local frame = CreateFrame("Frame", nil, UIParent)
-	frame:SetWidth(300)
+	frame:SetWidth(328)
 	frame:SetHeight(26)
 	frame:SetBackdrop(backdrop)
 	frame:SetBackdropColor(0, 0, 0, .9)
@@ -143,8 +143,10 @@ local function CreateRollFrame()
 
 	local need, needtext = CreateRollButton(frame, "Interface\\Buttons\\UI-GroupLoot-Dice-Up", "Interface\\Buttons\\UI-GroupLoot-Dice-Highlight", "Interface\\Buttons\\UI-GroupLoot-Dice-Down", 1, NEED, "LEFT", frame.button, "RIGHT", 5, -1)
 	local greed, greedtext = CreateRollButton(frame, "Interface\\Buttons\\UI-GroupLoot-Coin-Up", "Interface\\Buttons\\UI-GroupLoot-Coin-Highlight", "Interface\\Buttons\\UI-GroupLoot-Coin-Down", 2, GREED, "LEFT", need, "RIGHT", 0, -1)
-	local pass, passtext = CreateRollButton(frame, "Interface\\Buttons\\UI-GroupLoot-Pass-Up", nil, "Interface\\Buttons\\UI-GroupLoot-Pass-Down", 0, PASS, "LEFT", greed, "RIGHT", 0, 2.2)
-	frame.need, frame.greed, frame.pass = needtext, greedtext, passtext
+	local de, detext = CreateRollButton(frame, "Interface\\Buttons\\UI-GroupLoot-DE-Down", "Interface\\Buttons\\UI-GroupLoot-DE-Highlight", "Interface\\Buttons\\UI-GroupLoot-DE-Down", 3, ROLL_DISENCHANT, "LEFT", greed, "RIGHT", 0, -1)
+	local pass, passtext = CreateRollButton(frame, "Interface\\Buttons\\UI-GroupLoot-Pass-Up", nil, "Interface\\Buttons\\UI-GroupLoot-Pass-Down", 0, PASS, "LEFT", de, "RIGHT", 0, 2.2)
+	frame.needbutt, frame.greedbutt, frame.disenchantbutt = need, greed, de
+	frame.need, frame.greed, frame.pass, frame.disenchant = needtext, greedtext, passtext, detext
 
 	local bind = frame:CreateFontString()
 	bind:SetPoint("LEFT", pass, "RIGHT", 3, 1)
@@ -185,6 +187,11 @@ anchor:RegisterForClicks("RightButtonUp")
 anchor:Hide()
 
 local frames = {}
+
+local f = CreateRollFrame() -- Create one for good measure
+f:SetPoint("TOPLEFT", next(frames) and frames[#frames] or anchor, "BOTTOMLEFT", 0, -4)
+table.insert(frames, f)
+
 local function GetFrame()
 	for i,f in ipairs(frames) do
 		if not f.rollid then return f end
@@ -207,10 +214,19 @@ local function START_LOOT_ROLL(rollid, time)
 	f.need:SetText(0)
 	f.greed:SetText(0)
 	f.pass:SetText(0)
+	f.disenchant:SetText(0)
 
-	local texture, name, count, quality, bop = GetLootRollItemInfo(rollid)
+	local texture, name, count, quality, bop, canNeed, canGreed, canDisenchant = GetLootRollItemInfo(rollid)
 	f.button:SetNormalTexture(texture)
 	f.button.link = GetLootRollItemLink(rollid)
+
+	if canNeed then f.needbutt:Enable() else f.needbutt:Disable() end
+	if canGreed then f.greedbutt:Enable() else f.greedbutt:Disable() end
+	if canDisenchant then f.disenchantbutt:Enable() else f.disenchantbutt:Disable() end
+	SetDesaturation(f.needbutt:GetNormalTexture(), not canNeed)
+	SetDesaturation(f.greedbutt:GetNormalTexture(), not canGreed)
+	SetDesaturation(f.disenchantbutt:GetNormalTexture(), not canDisenchant)
+
 
 	f.fsbind:SetText(bop and "BoP" or "BoE")
 	f.fsbind:SetVertexColor(bop and 1 or .3, bop and .3 or 1, bop and .1 or .3)
@@ -252,6 +268,7 @@ local rollpairs = locale == "deDE" and {
 	["^(.*) passed on: (.+|r)$"]  = "pass",
 	["(.*) has selected Greed for: (.+)"] = "greed",
 	["(.*) has selected Need for: (.+)"]  = "need",
+	["(.*) has selected Disenchant for: (.+)"]  = "disenchant",
 }
 local function ParseRollChoice(msg)
 	for i,v in pairs(rollpairs) do
